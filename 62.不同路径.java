7c4f9f1c86b155c45f03858a9e08db01394cb8f8
huatep122 <huatep122@163.com>
2019-10-30 08:42:14 +0800

    62

--- 62.不同路径.java
+++ 62.不同路径.java
@@ -6,25 +6,25 @@
 
 // @lc code=start
 class Solution {
-    // O(m*n)
-    // public int uniquePaths(int m, int n) {
-    //     int[][] dp = new int[m][n];
-    //     for (int i = 0; i < m; i++){
-    //         dp[i][0] = 1;
-    //     }
-    //     for (int i = 0; i < n; i++){
-    //         dp[0][i] = 1;
-    //     }
-    //     for (int i = 1; i < m; i++){
-    //         for (int j = 1; j <n; j++){
-    //             dp[i][j] = dp[i-1][j] + dp[i][j-1];
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-    // }
-
-    // O(n);
+    //O(m*n)
     public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j <n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    //O(n);
+    public int uniquePaths1(int m, int n) {
         int[] dp = new int[n];
         for (int i =0; i < n; i++){
             dp[i] = 1;
@@ -36,6 +36,7 @@ class Solution {
         }
         return dp[n-1];
     }
+        
 }
 // @lc code=end
 
