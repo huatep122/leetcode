800fada470b2b192db3ffb98aed54ae5e7bfbb8e
huatep122 <huatep122@163.com>
2019-12-03 20:03:06 +0800

    112

--- 112.路径总和.java
+++ 112.路径总和.java
@@ -10,12 +10,39 @@
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 class Solution {
+    // public boolean hasPathSum(TreeNode root, int sum) {
+    //     if (root == null)
+    //         return false;
+    //     if (sum == root.val && root.left == null && root.right == null)
+    //         return true;
+    //     return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    // }
+
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null)
             return false;
-        if (sum == root.val && root.left == null && root.right == null)
-            return true;
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        Deque<TreeNode> nodeStack = new LinkedList<>();
+        Deque<Integer> sumStack = new LinkedList<>();
+
+        nodeStack.push(root);
+        sumStack.push(sum);
+        while (!nodeStack.isEmpty()) {
+            root = nodeStack.pop();
+            sum = sumStack.pop();
+
+            if (root.val == sum && root.left == null && root.right == null) {
+                return true;
+            }
+            if (root.right != null) {
+                nodeStack.push(root.right);
+                sumStack.push(sum - root.val);
+            }
+            if (root.left != null) {
+                nodeStack.push(root.left);
+                sumStack.push(sum - root.val);
+            }
+        }
+        return false;
     }
 }
 // @lc code=end
---
+++ 113.路径总和-ii.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=java
+ *
+ * [113] 路径总和 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        return help(result, list, root, sum);
+    }
+
+    public List<List<Integer>> help(List<List<Integer>> result, List<Integer> list, TreeNode root, int sum) {
+        if (root == null)
+            return result;
+        if (sum == root.val && root.left == null && root.right == null) {
+            list.add(root.val);
+            List<Integer> l = clone(list);
+            result.add(l);
+            list.remove(list.size()-1);
+        }
+        list.add(root.val);
+        result = help(result, list, root.left, sum-root.val);
+        result = help(result, list, root.right, sum-root.val);
+        list.remove(list.size()-1);
+        return result;
+    }
+
+    public List<Integer> clone(List<Integer> list) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer val : list) {
+            result.add(val);
+        }
+        return result;
+    }
+}
+// @lc code=end
