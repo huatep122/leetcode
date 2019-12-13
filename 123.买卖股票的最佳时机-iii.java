e754dc557a01942f2cdca9a202f2c53d3fa36724
huatep122 <huatep122@163.com>
2019-12-13 15:44:27 +0800

    123

---
+++ 123.买卖股票的最佳时机-iii.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=java
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int[][][] dp = new int[prices.length][3][2];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if (i == 0) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[0];
+                } else {
+                    dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+                }
+            }
+        }
+        return dp[prices.length - 1][2][0];
+    }
+}
+// @lc code=end
