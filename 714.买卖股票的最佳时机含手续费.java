b9ca7065c6eddaf953a3bd54308342f1917bfe6e
huatep122 <huatep122@163.com>
2019-12-13 15:58:16 +0800

    714

---
+++ 714.买卖股票的最佳时机含手续费.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=714 lang=java
+ *
+ * [714] 买卖股票的最佳时机含手续费
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -(prices[0] + fee);
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+        return dp[prices.length - 1][0];
+    }
+}
+// @lc code=end
+
