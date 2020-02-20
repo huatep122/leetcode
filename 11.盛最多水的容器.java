94d5ef7182b68e54a5b6e59c277ff05f22348ccf
huatep122 <huatep122@163.com>
2020-02-20 20:53:12 +0800

    11

---
+++ 11.盛最多水的容器.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+    
+}
+// @lc code=end
+
