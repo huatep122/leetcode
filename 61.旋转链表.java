1590b43242d4e1597f2a781c12bfeed66c741074
huatep122 <huatep122@163.com>
2020-02-21 17:18:53 +0800

    61

---
+++ 61.旋转链表.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=java
+ *
+ * [61] 旋转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return head;
+        int len = 0;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            fast = fast.next;
+            len++;
+        }
+        fast = head;
+        k = k % len;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+        return head;
+    }
+}
+// @lc code=end
+
